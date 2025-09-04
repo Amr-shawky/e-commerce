bc7b5ca0d49501e03e7d47965ab151599ff08d7d
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-09-04 15:56:44 +0300

    Add schematics for Angular components and services; update dependencies and integrate ng-icons

--- src/app/features/user/pages/home/home.ts
+++ src/app/features/user/pages/home/home.ts
@@ -1,12 +1,16 @@
-import { ProductCard } from './../../components/product-card/product-card';
 import { Component } from '@angular/core';
+import { ProductCard } from './../../components/product-card/product-card';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import {faCalendarCheck} from '@ng-icons/font-awesome/regular'
 
 @Component({
   selector: 'app-home',
-  imports: [ProductCard],
+  imports: [ProductCard , NgIcon],
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrl: './home.css',
+    viewProviders: [provideIcons({faCalendarCheck })]
 })
 export class Home {
   products = [1,2,3,4,5,2,5,6]
 }
+//
