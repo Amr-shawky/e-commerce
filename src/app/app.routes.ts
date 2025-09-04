a0fbbddff9992528da66917f18ea01f20332206a
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-09-04 17:49:18 +0300

    feat: update routing configuration to include all user feature pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,15 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
 
-    { path: '', loadComponent: () => import('./features/user/pages/home/home').then(m => m.Home) }
+    { path: 'home', loadComponent: () => import('./features/user/pages/home/home').then(m => m.Home) },
+    { path:'',redirectTo:'home',pathMatch:'full'},
+    { path:'products',loadComponent:() =>import('./features/user/pages/products/products').then(m => m.Products)},
+    { path:'login', loadComponent:() =>import('./features/user/pages/login/login').then(m => m.Login)},
+    { path:'register', loadComponent:() =>import('./features/user/pages/register/register').then(m => m.Register)},
+    { path:'product/:id', loadComponent:() =>import('./features/user/pages/product-details/product-details').then(m => m.ProductDetails)},
+    { path:'cart', loadComponent:() =>import('./features/user/pages/cart/cart').then(m => m.Cart)},
+    { path:'brands', loadComponent:() =>import('./features/user/pages/brands/brands').then(m => m.Brands)},
+    { path:'categories', loadComponent:() =>import('./features/user/pages/categories/categories').then(m => m.Categories)},
+    { path:'reset-password', loadComponent:() =>import('./features/user/pages/reset-password/reset-password').then(m => m.ResetPassword)},
+    { path:'**', loadComponent:() =>import('./features/user/pages/notfound/notfound').then(m => m.Notfound)}
 ];
