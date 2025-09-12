751b658eda02f344e9f6dcb5afe6aab067567acc
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-09-12 17:08:26 +0300

    feat: implement register form with reactive validation and dynamic input fields

--- src/app/features/user/pages/register/register.ts
+++ src/app/features/user/pages/register/register.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {TitleCasePipe} from '@angular/common';
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [TitleCasePipe, ReactiveFormsModule],
   templateUrl: './register.html',
-  styleUrl: './register.css'
+  styleUrls: ['./register.css']
 })
 export class Register {
+// {
+//     "name": "Ahmed Abd Al-Muti",
+//     "email":"ahmedmuttii4012@gmail.com",
+//     "password":"Ahmed@123",
+//     "rePassword":"Ahmed@123",
+//     "phone":"01010700701"
+// }
+  registerform = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    rePassword: new FormControl(''),
+    phone: new FormControl('')
+  });
 
+  handlesubmit() {
+    console.log(this.registerform.value);
+  }
 }
