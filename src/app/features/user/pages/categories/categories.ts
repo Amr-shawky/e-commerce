bd0eff41795a1cc40ed787e201d027e85ef0ba31
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-09-14 15:23:30 +0300

    feat: implement categories page with dynamic category listing and data fetching

--- src/app/features/user/pages/categories/categories.ts
+++ src/app/features/user/pages/categories/categories.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../../../../core/services/category.service';
+import { Category } from '../../../../core/models/api.interface';
 
 @Component({
   selector: 'app-categories',
@@ -6,6 +8,21 @@ import { Component } from '@angular/core';
   templateUrl: './categories.html',
   styleUrl: './categories.css'
 })
-export class Categories {
-
+export class Categories implements OnInit {
+  constructor(private categoryService: CategoryService) {}
+  Categories: Category[] = [];
+  ngOnInit() {
+    this.getCategories();
+  }
+  getCategories() {
+    return this.categoryService.getAllCategories({}).subscribe({
+      next: (categories) => {
+        this.Categories = categories.data;
+        console.log(categories);
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+}
 }
