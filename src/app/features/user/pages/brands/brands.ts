3e5dee7e4acd9700e37d8e77f8c21d5e8a321b5c
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-09-14 15:16:58 +0300

    feat: implement brands page with dynamic brand listing and data fetching

--- src/app/features/user/pages/brands/brands.ts
+++ src/app/features/user/pages/brands/brands.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BrandService } from '../../../../core/services/brand.service';
+import { error } from 'console';
+import { Brand } from '../../../../core/models/api.interface';
 
 @Component({
   selector: 'app-brands',
@@ -6,6 +9,22 @@ import { Component } from '@angular/core';
   templateUrl: './brands.html',
   styleUrl: './brands.css'
 })
-export class Brands {
+export class Brands implements OnInit {
+  constructor(private brandService: BrandService) {}
 
+  Brands:Brand[]=[];
+  getbrands() {
+    return this.brandService.getAllBrands({limit: 40,page: 1}).subscribe({
+      next: (brands) => {
+        console.log(brands.data);
+        this.Brands = brands.data;
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    }); 
+  }
+  ngOnInit() {
+    this.getbrands();
+  }
 }
