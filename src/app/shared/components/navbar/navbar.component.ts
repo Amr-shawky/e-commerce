36e73fb515f56c7b84cd6efd7faf35d2b5b1f75b
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-09-15 13:02:11 +0300

    feat: add cart item count to navbar and fetch cart data on initialization

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { FlowbiteService } from '../../../core/services/flowbite';
+import { CartService } from '../../../core/services/cart.service';
+import { CartResponse } from '../../../core/models/api.interface';
 
 @Component({
   selector: 'app-navbar',
@@ -12,7 +14,10 @@ import { FlowbiteService } from '../../../core/services/flowbite';
 })
 
 export class NavbarComponent implements OnInit{
-  constructor(private authService: AuthService, private flowbiteService: FlowbiteService) {
+  constructor(private authService: AuthService, 
+    private flowbiteService: FlowbiteService,
+    private cartservice : CartService
+  ) {
      authService.isLogin.subscribe({
       next: (isLogin) => {
         console.log({isLogin} , "nav");
@@ -22,13 +27,33 @@ export class NavbarComponent implements OnInit{
     });
   }
     localIsLogin = false;
+    numOfCartItems : number = 0;
   logOut() {
     this.authService.logOut();
   }
+    getallcarts() {
+  
+      this.cartservice.getCart().subscribe({
+        next: (response: CartResponse) => {
+          this.numOfCartItems = response.numOfCartItems;
+          // this.cartData = response;
+          // this.isFirstLoading = false;
+          // this.isLoading = false;
+        },
+        error: (error: any) => {
+          console.log(error);
+          // this.toastr.error('Failed to load cart', 'Error');
+          // this.isFirstLoading = false;
+          // this.isLoading = false;
+        },
+      });
+    }
+  
   ngOnInit(): void {
     this.flowbiteService.loadFlowbite((flowbite) => {
       initFlowbite();
     });
+    this.getallcarts();
   }
   pages:{title:string, path:string}[] = [
     {title: 'Home', path: '/home'},
