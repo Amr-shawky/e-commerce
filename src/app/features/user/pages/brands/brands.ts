ae728429169e254f05d0fd7cf809487e284ce926
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-09-18 16:34:54 +0300

    feat: Enhance brands and categories components with dark mode support and loading skeletons

--- src/app/features/user/pages/brands/brands.ts
+++ src/app/features/user/pages/brands/brands.ts
@@ -1,30 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, inject } from '@angular/core';
 import { BrandService } from '../../../../core/services/brand.service';
-import { error } from 'console';
+import { ModeService } from '../../../../core/services/mode.service';
 import { Brand } from '../../../../core/models/api.interface';
+import { SpinnerComponent } from '../../../../shared/components/spinner/spinner.component';
+import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-brands',
-  imports: [],
+  standalone: true,
+  imports: [SpinnerComponent,CommonModule],
   templateUrl: './brands.html',
-  styleUrl: './brands.css'
+  styleUrls: ['./brands.css'],
 })
-export class Brands implements OnInit {
-  constructor(private brandService: BrandService) {}
+export class Brands implements OnInit, OnDestroy {
+  Brands: Brand[] = [];
+  isFirstLoading: boolean = true;
+  isLoading: boolean = false;
+  private modeSubscription!: Subscription;
+  private brandService = inject(BrandService);
+  private modeService = inject(ModeService);
 
-  Brands:Brand[]=[];
+  ngOnInit() {
+    this.modeSubscription = this.modeService.mode.subscribe((mode) => {
+      console.log('BrandsComponent: Mode changed to:', mode); // Debugging
+    });
+    this.getbrands();
+  }
+
+  ngOnDestroy(): void {
+    if (this.modeSubscription) {
+      this.modeSubscription.unsubscribe();
+    }
+  }
+
+  get isDarkMode(): boolean {
+    return this.modeService.mode.value === 'dark';
+  }
+
   getbrands() {
-    return this.brandService.getAllBrands({limit: 40,page: 1}).subscribe({
+    if (!this.Brands.length) {
+      this.isFirstLoading = true; // Show skeleton on initial load
+    } else {
+      this.isLoading = true; // Show spinner for updates
+    }
+    this.brandService.getAllBrands({ limit: 40, page: 1 }).subscribe({
       next: (brands) => {
         console.log(brands.data);
         this.Brands = brands.data;
+        this.isFirstLoading = false;
+        this.isLoading = false;
       },
       error: (error) => {
         console.error(error);
-      }
-    }); 
-  }
-  ngOnInit() {
-    this.getbrands();
+        this.isFirstLoading = false;
+        this.isLoading = false;
+      },
+    });
   }
 }
--- src/app/features/user/pages/categories/categories.ts
+++ src/app/features/user/pages/categories/categories.ts
@@ -1,28 +1,60 @@
-import { Component, OnInit } from '@angular/core';
-import { CategoryService } from '../../../../core/services/category.service';
+import { Component, OnInit, OnDestroy, inject } from '@angular/core';
+import { ModeService } from '../../../../core/services/mode.service';
 import { Category } from '../../../../core/models/api.interface';
-
+import { SpinnerComponent } from '../../../../shared/components/spinner/spinner.component';
+import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { CategoryService } from '../../../../core/services/category.service';
 @Component({
   selector: 'app-categories',
-  imports: [],
+  standalone: true,
+  imports: [SpinnerComponent,CommonModule],
   templateUrl: './categories.html',
-  styleUrl: './categories.css'
+  styleUrls: ['./categories.css'],
 })
-export class Categories implements OnInit {
-  constructor(private categoryService: CategoryService) {}
+export class Categories implements OnInit, OnDestroy {
   Categories: Category[] = [];
+  isFirstLoading: boolean = true;
+  isLoading: boolean = false;
+  private modeSubscription!: Subscription;
+  private categoryService = inject(CategoryService);
+  private modeService = inject(ModeService);
+
   ngOnInit() {
+    this.modeSubscription = this.modeService.mode.subscribe((mode) => {
+      console.log('CategoriesComponent: Mode changed to:', mode); // Debugging
+    });
     this.getCategories();
   }
+
+  ngOnDestroy(): void {
+    if (this.modeSubscription) {
+      this.modeSubscription.unsubscribe();
+    }
+  }
+
+  get isDarkMode(): boolean {
+    return this.modeService.mode.value === 'dark';
+  }
+
   getCategories() {
-    return this.categoryService.getAllCategories({}).subscribe({
+    if (!this.Categories.length) {
+      this.isFirstLoading = true; // Show skeleton on initial load
+    } else {
+      this.isLoading = true; // Show spinner for updates
+    }
+    this.categoryService.getAllCategories({}).subscribe({
       next: (categories) => {
         this.Categories = categories.data;
         console.log(categories);
+        this.isFirstLoading = false;
+        this.isLoading = false;
       },
       error: (error) => {
         console.error(error);
-      }
+        this.isFirstLoading = false;
+        this.isLoading = false;
+      },
     });
-}
+  }
 }
