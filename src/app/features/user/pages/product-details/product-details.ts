ae11bea6013c5fcebb490d1b5e8331151899c138
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-09-20 15:36:40 +0300

    fix: remove unnecessary 'xl:grid-cols-6' class for improved layout in multiple components

--- src/app/features/user/pages/product-details/product-details.ts
+++ src/app/features/user/pages/product-details/product-details.ts
@@ -1,44 +1,55 @@
+import { Component, OnInit, OnDestroy, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../../../../core/models/api.interface';
-import { ProductService } from './../../../../core/services/product.service';
-import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../../../core/services/product.service';
+import { CartService } from '../../../../core/services/cart.service';
+import { ModeService } from '../../../../core/services/mode.service';
+import { ToastrService } from 'ngx-toastr';
 import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
-
+import { SpinnerComponent } from '../../../../shared/components/spinner/spinner.component';
+import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-details',
-  imports: [CarouselModule],
+  standalone: true,
+  imports: [CarouselModule, SpinnerComponent,CommonModule],
   templateUrl: './product-details.html',
-  styleUrls: ['./product-details.css']
+  styleUrls: ['./product-details.css'],
 })
-
-export class ProductDetails implements OnInit {
+export class ProductDetails implements OnInit, OnDestroy {
   product: Product | null = null;
+  isFirstLoading: boolean = true;
   isLoading: boolean = false;
   errorMessage: string = '';
   currentImageIndex: number = 0;
+  private modeSubscription!: Subscription;
+  private productService = inject(ProductService);
+  private cartService = inject(CartService);
+  private toastr = inject(ToastrService);
+  private route = inject(ActivatedRoute);
+  private modeService = inject(ModeService);
 
-  constructor(
-    private productService: ProductService,
-    private route: ActivatedRoute
-  ) {}
-carouselOptions = {
-  loop: true,
-  margin: 10,
-  nav: true,
-  dots: true,
-  items: 1,
-  autoplay: true,
-  autoplayTimeout: 2000,
-  autoplayHoverPause: true,
-  navText: [
-    '<i class="fa-regular fa-circle-left text-2xl"></i>',
-    '<i class="fa-regular fa-circle-right text-2xl"></i>'
-  ]
-};
+  carouselOptions: OwlOptions = {
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: true,
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    navText: [
+      '<i class="fa-regular fa-circle-left text-2xl"></i>',
+      '<i class="fa-regular fa-circle-right text-2xl"></i>',
+    ],
+  };
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
+    this.modeSubscription = this.modeService.mode.subscribe((mode) => {
+      console.log('ProductDetailsComponent: Mode changed to:', mode); // Debugging
+    });
+    this.route.paramMap.subscribe((params) => {
       const id = params.get('id');
       if (id) {
         this.getProductDetails(id);
@@ -46,18 +57,47 @@ carouselOptions = {
     });
   }
 
+  ngOnDestroy(): void {
+    if (this.modeSubscription) {
+      this.modeSubscription.unsubscribe();
+    }
+  }
+
+  get isDarkMode(): boolean {
+    return this.modeService.mode.value === 'dark';
+  }
+
   getProductDetails(id: string) {
+    this.isFirstLoading = true;
     this.isLoading = true;
     this.productService.getProductDetails(id).subscribe({
       next: (response: { data: Product | null }) => {
-        this.product = response.data; 
+        this.product = response.data;
+        this.isFirstLoading = false;
         this.isLoading = false;
       },
       error: (error: any) => {
         this.errorMessage = 'Error fetching product details';
         console.error(error);
+        this.isFirstLoading = false;
         this.isLoading = false;
-      }
+      },
+    });
+  }
+
+  addToCart(productId: string) {
+    this.isLoading = true;
+    this.cartService.addProductToCart(productId).subscribe({
+      next: (response) => {
+        this.toastr.success('Product added to cart successfully', 'Success');
+        this.cartService.numOfCartItems.next(response.numOfCartItems);
+        this.isLoading = false;
+      },
+      error: (error) => {
+        this.toastr.error('Failed to add product to cart', 'Error');
+        console.error(error);
+        this.isLoading = false;
+      },
     });
   }
 
